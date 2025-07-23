99cb0785b5436837000904a01594fb1c5c5458d7
Weronika Grzeszczyk <werawera112@gmail.com>
2025-07-23 13:23:15 +0200

    remove changes

--- android/src/main/java/com/reactnativestandalonevideoplayer/PlayerVideo.kt
+++ android/src/main/java/com/reactnativestandalonevideoplayer/PlayerVideo.kt
@@ -117,11 +117,6 @@ class PlayerVideo(private val context: Context) {
     startProgressTimer()
   }
 
-  fun setVolume(volume: Float) {
-    Log.d("PlayerVideo", "setVolume")
-    volume = volume
-  }
-
   fun play() {
     Log.d("PlayerVideo", "play")
     if (status == PlayerVideoStatus.finished) {
